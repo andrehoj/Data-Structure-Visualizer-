489f4712c6a1ab0a6b8a5d5f16cb6ac66591e72d
Andrew Howran <andrewhowrandev17@gmail.com>
2024-02-16 21:02:38 -0500

    added ll length setter method and jquery

--- dist/index.js
+++ dist/index.js
@@ -14,145 +14,79 @@ var LinkedList = /** @class */ (function () {
         var _this = this;
         this.head = null;
         this.size = 0;
-        this.llContainer = document.getElementById('ll-container');
-        this.insertNodeAtIndexBtn = document.getElementById("insert-at-location");
-        this.insertNodeAtIndexBtn.addEventListener('click', function (e) {
+        this.sizeDisplay = $("#size-display")[0];
+        this.sizeDisplay.textContent = this.size.toString();
+        // Select all elements with class name 'node_container' inside the div with id 'll-container'
+        // Selecting llContainer using jQuery
+        this.llContainer = $("#ll-container")[0]; // Using [0] to access the first element in the jQuery object as a plain DOM element
+        // Selecting insertNodeAtIndexBtn using jQuery
+        this.insertNodeAtIndexBtn = $("#insert-at-location")[0]; // Using [0] to access the first element in the jQuery object as a plain DOM element
+        // Adding event listener using jQuery
+        $(this.insertNodeAtIndexBtn).on('click', function (e) {
             e.stopPropagation();
-            var valueInput = document.getElementById("insert-value-input");
-            var value = parseInt(valueInput.value);
-            var indexInput = document.getElementById("insert-index-input");
-            var index = parseInt(indexInput.value);
+            var valueInput = $("#insert-value-input");
+            var value = parseInt(valueInput.val());
+            var indexInput = $("#insert-index-input");
+            var index = parseInt(indexInput.val());
             _this.insertAtIndex(value, index);
         });
     }
+    LinkedList.prototype.updateSize = function () {
+        this.size++;
+        this.sizeDisplay.textContent = this.size.toString();
+    };
     // adds an node to the end of the list
     LinkedList.prototype.insertAtIndex = function (value, index) {
-        var _a;
         if (!value)
-            throw new Error("invalid value");
+            throw new Error("Invalid value");
         try {
             var domNode = this.createLLDomNode(value);
-            var tailNode = document.getElementById("tail_node");
-            // if length is 2, there is only head and tail
-            if (this.llContainer.children.length === 2) {
-                this.llContainer.insertBefore(domNode, tailNode);
+            var nodesInLL = $(this.llContainer).children('.node_container');
+            ;
+            // // if length is 2, there is only head and tail
+            if (nodesInLL.length === 0) {
+                $(this.llContainer).children().first().after(domNode);
             }
             else {
                 if (index < 0 || index > this.size + 1)
                     throw new Error("invalid linkedList index");
-                (_a = this.llContainer) === null || _a === void 0 ? void 0 : _a.insertBefore(domNode, this.llContainer.children[index === 0 ? 1 : index]);
+                $(this.llContainer.children[index === 0 ? 1 : index]).before(domNode);
             }
-            this.size++;
+            this.updateSize();
         }
         catch (error) {
             console.log(error);
         }
     };
     ;
+    LinkedList.prototype.removeElement = function () { };
+    LinkedList.prototype.removeFrom = function () { };
     LinkedList.prototype.createLLDomNode = function (value) {
         // Create the main container div
-        var nodeContainer = document.createElement("div");
-        nodeContainer.classList.add("node_container");
+        var nodeContainer = $("<div>").addClass("node_container");
         // Create inner container div
-        var innerContainer = document.createElement("div");
-        innerContainer.classList.add("node_inner_container");
+        var innerContainer = $("<div>").addClass("node_inner_container");
         // Create node data div
-        var nodeData = document.createElement("div");
-        nodeData.classList.add("node_data");
-        var paragraph = document.createElement("p");
-        paragraph.textContent = value.toString();
-        nodeData.appendChild(paragraph);
+        var nodeData = $("<div>").addClass("node_data");
+        var paragraph = $("<p>").text(value.toString());
+        nodeData.append(paragraph);
         // Create next container div
-        var nextContainer = document.createElement("div");
-        nextContainer.classList.add("node_next_container");
+        var nextContainer = $("<div>").addClass("node_next_container");
         // Create next node div
-        var nextNode = document.createElement("div");
-        nextNode.classList.add("node_next");
+        var nextNode = $("<div>").addClass("node_next");
         // Create arrow container div
-        var arrowContainer = document.createElement("div");
-        arrowContainer.classList.add("arrow-container");
+        var arrowContainer = $("<div>").addClass("arrow-container");
         // Create arrow div
-        var arrow = document.createElement("div");
-        arrow.classList.add("arrow");
-        var line = document.createElement("div");
-        line.classList.add("line");
+        var arrow = $("<div>").addClass("arrow");
+        var line = $("<div>").addClass("line");
         // Append elements to their respective parents
-        arrowContainer.appendChild(arrow);
-        arrowContainer.appendChild(line);
-        nextNode.appendChild(arrowContainer);
-        nextContainer.appendChild(nextNode);
-        innerContainer.appendChild(nodeData);
-        innerContainer.appendChild(nextContainer);
-        nodeContainer.appendChild(innerContainer);
+        arrowContainer.append(arrow, line);
+        nextNode.append(arrowContainer);
+        nextContainer.append(nextNode);
+        innerContainer.append(nodeData, nextContainer);
+        nodeContainer.append(innerContainer);
         return nodeContainer;
     };
     return LinkedList;
 }());
-// function createLinkedListNode(data: number) {
-//     // Create the main container div
-//     const nodeContainer = document.createElement("div");
-//     nodeContainer.classList.add("node_container");
-//     // Create inner container div
-//     const innerContainer = document.createElement("div");
-//     innerContainer.classList.add("node_inner_container");
-//     // Create node data div
-//     const nodeData = document.createElement("div");
-//     nodeData.classList.add("node_data");
-//     const paragraph = document.createElement("p");
-//     paragraph.textContent = data.toString();
-//     nodeData.appendChild(paragraph);
-//     // Create next container div
-//     const nextContainer = document.createElement("div");
-//     nextContainer.classList.add("node_next_container");
-//     // Create next node div
-//     const nextNode = document.createElement("div");
-//     nextNode.classList.add("node_next");
-//     // Create arrow container div
-//     const arrowContainer = document.createElement("div");
-//     arrowContainer.classList.add("arrow-container");
-//     // Create arrow div
-//     const arrow = document.createElement("div");
-//     arrow.classList.add("arrow");
-//     const line = document.createElement("div");
-//     line.classList.add("line");
-//     // Append elements to their respective parents
-//     arrowContainer.appendChild(arrow);
-//     arrowContainer.appendChild(line);
-//     nextNode.appendChild(arrowContainer);
-//     nextContainer.appendChild(nextNode);
-//     innerContainer.appendChild(nodeData);
-//     innerContainer.appendChild(nextContainer);
-//     nodeContainer.appendChild(innerContainer);
-//     return nodeContainer;
-// }
-// addNodeBtnEnd?.addEventListener("click", function (e) {
-//     e.stopPropagation();
-//     const node = createLinkedListNode(Math.floor(Math.random() * 999));
-//     const childNodes = document.getElementById("ll-container")?.childNodes;
-//     if (childNodes?.length)
-//         linkedListContainer?.insertBefore(node, childNodes[childNodes?.length - 2]);
-// });
-// addNodeBtnStart?.addEventListener("click", (e) => {
-//     e.stopPropagation();
-//     const node = createLinkedListNode(Math.floor(Math.random() * 999));
-//     const firstChild = document.getElementById("ll-container")?.childNodes[2];
-//     linkedListContainer?.insertBefore(node, firstChild as ChildNode);
-// });
-// insertNodeAtIndexBtn?.addEventListener("click", (e) => {
-//     e.stopPropagation();
-//     const valueInput = document.getElementById(
-//         "insert-value-input"
-//     ) as HTMLInputElement;
-//     const value = parseInt(valueInput.value);
-//     const indexInput = document.getElementById(
-//         "insert-index-input"
-//     ) as HTMLInputElement;
-//     const index = parseInt(indexInput.value);
-//     insertNodeAtIndex(index, value);
-// });
-// function insertNodeAtIndex(index: number, value: number) {
-//     const node = createLinkedListNode(value);
-//     const childNodes = document.getElementById("ll-container")?.children;
-//     if (childNodes) linkedListContainer?.insertBefore(node, childNodes[index]);
-// }
 var ll = new LinkedList();
--- src/index.ts
+++ src/index.ts
@@ -22,176 +22,83 @@ class LinkedList {
     size: number
     llContainer: HTMLDivElement
     insertNodeAtIndexBtn: HTMLElement
-
-
+    sizeDisplay: HTMLSpanElement
     constructor() {
         this.head = null
         this.size = 0;
-        this.llContainer = document.getElementById('ll-container') as HTMLDivElement;
-        this.insertNodeAtIndexBtn = document.getElementById("insert-at-location") as HTMLButtonElement;
-        this.insertNodeAtIndexBtn.addEventListener('click', (e) => {
+        this.sizeDisplay = $("#size-display")[0] as HTMLSpanElement;
+        this.sizeDisplay.textContent = this.size.toString();
+        // Select all elements with class name 'node_container' inside the div with id 'll-container'
+        // Selecting llContainer using jQuery
+        this.llContainer = $("#ll-container")[0] as HTMLDivElement; // Using [0] to access the first element in the jQuery object as a plain DOM element
+        // Selecting insertNodeAtIndexBtn using jQuery
+        this.insertNodeAtIndexBtn = $("#insert-at-location")[0]; // Using [0] to access the first element in the jQuery object as a plain DOM element
+
+        // Adding event listener using jQuery
+        $(this.insertNodeAtIndexBtn).on('click', (e) => {
             e.stopPropagation();
-            const valueInput = document.getElementById(
-                "insert-value-input"
-            ) as HTMLInputElement;
-            const value = parseInt(valueInput.value);
-
-            const indexInput = document.getElementById(
-                "insert-index-input"
-            ) as HTMLInputElement;
-            const index = parseInt(indexInput.value);
-
+            const valueInput = $("#insert-value-input");
+            const value = parseInt(valueInput.val() as string);
+            const indexInput = $("#insert-index-input");
+            const index = parseInt(indexInput.val() as string);
             this.insertAtIndex(value, index);
-
         });
+    }
+
+    updateSize() {
+        this.size++;
+        this.sizeDisplay.textContent = this.size.toString();
     }
 
     // adds an node to the end of the list
     insertAtIndex(value: number, index: number) {
-        if (!value) throw new Error("invalid value")
+        if (!value) throw new Error("Invalid value")
         try {
-            const domNode = this.createLLDomNode(value)
-
-            const tailNode = document.getElementById("tail_node")
-
-            // if length is 2, there is only head and tail
-            if (this.llContainer.children.length === 2) { this.llContainer.insertBefore(domNode, tailNode) } else {
+            const domNode = this.createLLDomNode(value);
+            const nodesInLL = $(this.llContainer).children('.node_container');;
+            // // if length is 2, there is only head and tail
+            if (nodesInLL.length === 0) {
+                $(this.llContainer).children().first().after(domNode);
+            } else {
                 if (index < 0 || index > this.size + 1) throw new Error("invalid linkedList index");
-                this.llContainer?.insertBefore(domNode, this.llContainer.children[index === 0 ? 1 : index]);
+                $(this.llContainer.children[index === 0 ? 1 : index]).before(domNode);
             }
 
-            this.size++;
+            this.updateSize()
         } catch (error) {
             console.log(error)
         }
 
     };
 
+    removeElement() { }
+    removeFrom() { }
+
     private createLLDomNode(value: number) {
         // Create the main container div
-        const nodeContainer = document.createElement("div");
-        nodeContainer.classList.add("node_container");
+        const nodeContainer = $("<div>").addClass("node_container");
         // Create inner container div
-        const innerContainer = document.createElement("div");
-        innerContainer.classList.add("node_inner_container");
+        const innerContainer = $("<div>").addClass("node_inner_container");
         // Create node data div
-        const nodeData = document.createElement("div");
-        nodeData.classList.add("node_data");
-        const paragraph = document.createElement("p");
-        paragraph.textContent = value.toString();
-        nodeData.appendChild(paragraph);
+        const nodeData = $("<div>").addClass("node_data");
+        const paragraph = $("<p>").text(value.toString());
+        nodeData.append(paragraph);
         // Create next container div
-        const nextContainer = document.createElement("div");
-        nextContainer.classList.add("node_next_container");
+        const nextContainer = $("<div>").addClass("node_next_container");
         // Create next node div
-        const nextNode = document.createElement("div");
-        nextNode.classList.add("node_next");
+        const nextNode = $("<div>").addClass("node_next");
         // Create arrow container div
-        const arrowContainer = document.createElement("div");
-        arrowContainer.classList.add("arrow-container");
+        const arrowContainer = $("<div>").addClass("arrow-container");
         // Create arrow div
-        const arrow = document.createElement("div");
-        arrow.classList.add("arrow");
-        const line = document.createElement("div");
-        line.classList.add("line");
+        const arrow = $("<div>").addClass("arrow");
+        const line = $("<div>").addClass("line");
         // Append elements to their respective parents
-        arrowContainer.appendChild(arrow);
-        arrowContainer.appendChild(line);
-        nextNode.appendChild(arrowContainer);
-        nextContainer.appendChild(nextNode);
-        innerContainer.appendChild(nodeData);
-        innerContainer.appendChild(nextContainer);
-        nodeContainer.appendChild(innerContainer);
+        arrowContainer.append(arrow, line);
+        nextNode.append(arrowContainer);
+        nextContainer.append(nextNode);
+        innerContainer.append(nodeData, nextContainer);
+        nodeContainer.append(innerContainer);
         return nodeContainer;
     }
 }
-
-// function createLinkedListNode(data: number) {
-//     // Create the main container div
-//     const nodeContainer = document.createElement("div");
-//     nodeContainer.classList.add("node_container");
-
-//     // Create inner container div
-//     const innerContainer = document.createElement("div");
-//     innerContainer.classList.add("node_inner_container");
-
-//     // Create node data div
-//     const nodeData = document.createElement("div");
-//     nodeData.classList.add("node_data");
-//     const paragraph = document.createElement("p");
-//     paragraph.textContent = data.toString();
-//     nodeData.appendChild(paragraph);
-
-//     // Create next container div
-//     const nextContainer = document.createElement("div");
-//     nextContainer.classList.add("node_next_container");
-
-//     // Create next node div
-//     const nextNode = document.createElement("div");
-//     nextNode.classList.add("node_next");
-
-//     // Create arrow container div
-//     const arrowContainer = document.createElement("div");
-//     arrowContainer.classList.add("arrow-container");
-
-//     // Create arrow div
-//     const arrow = document.createElement("div");
-//     arrow.classList.add("arrow");
-//     const line = document.createElement("div");
-//     line.classList.add("line");
-
-//     // Append elements to their respective parents
-//     arrowContainer.appendChild(arrow);
-//     arrowContainer.appendChild(line);
-//     nextNode.appendChild(arrowContainer);
-//     nextContainer.appendChild(nextNode);
-//     innerContainer.appendChild(nodeData);
-//     innerContainer.appendChild(nextContainer);
-//     nodeContainer.appendChild(innerContainer);
-
-//     return nodeContainer;
-// }
-
-// addNodeBtnEnd?.addEventListener("click", function (e) {
-//     e.stopPropagation();
-//     const node = createLinkedListNode(Math.floor(Math.random() * 999));
-//     const childNodes = document.getElementById("ll-container")?.childNodes;
-
-//     if (childNodes?.length)
-//         linkedListContainer?.insertBefore(node, childNodes[childNodes?.length - 2]);
-// });
-
-// addNodeBtnStart?.addEventListener("click", (e) => {
-//     e.stopPropagation();
-//     const node = createLinkedListNode(Math.floor(Math.random() * 999));
-
-//     const firstChild = document.getElementById("ll-container")?.childNodes[2];
-//     linkedListContainer?.insertBefore(node, firstChild as ChildNode);
-// });
-
-// insertNodeAtIndexBtn?.addEventListener("click", (e) => {
-//     e.stopPropagation();
-//     const valueInput = document.getElementById(
-//         "insert-value-input"
-//     ) as HTMLInputElement;
-//     const value = parseInt(valueInput.value);
-
-//     const indexInput = document.getElementById(
-//         "insert-index-input"
-//     ) as HTMLInputElement;
-//     const index = parseInt(indexInput.value);
-
-//     insertNodeAtIndex(index, value);
-// });
-
-// function insertNodeAtIndex(index: number, value: number) {
-//     const node = createLinkedListNode(value);
-
-//     const childNodes = document.getElementById("ll-container")?.children;
-
-//     if (childNodes) linkedListContainer?.insertBefore(node, childNodes[index]);
-// }
-
-
 const ll = new LinkedList();
-
